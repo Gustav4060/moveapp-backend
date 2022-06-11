5364e211baf70c02011250452a8a7a61750f6cca
Gustav4060 <gustav4060eps@gmail.com>
2022-06-10 23:31:12 -0500

    c
    c

--- src/main/java/com/nttdata/model/Cuenta.java
+++ src/main/java/com/nttdata/model/Cuenta.java
@@ -16,7 +16,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.nttdata.enumeration.TipoCuentaEnum;
 
 import lombok.Data;
